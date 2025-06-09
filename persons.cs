8715a4fde24a23501711fd4d0cf85e48cf449a94
elazar matan <elazarmata8@gmail.com>
2025-06-09 15:39:13 +0300

    Add reports functionality and update persons class
    - Updated `malshinon.csproj` to include `reports.cs`.
    - Introduced `type` enum in `persons.cs` with relevant values.
    - Enhanced `persons` class with new properties and constructor.
    - Added `Reports` class in `reports.cs` with necessary properties and constructor.

--- persons.cs
+++ persons.cs
@@ -1,4 +1,29 @@
-﻿public class persons
+﻿
+public enum type
 {
+    reporter,
+    target,
+    both,
+    potential_agent
+}
+public class persons
+{
+    int id { get; set; }
+    string first_name { get; set; }
+    string last_name { get; set; }
+    string secret_code { get; set; }
+    type type { get; set; } = type.reporter;
+    int   num_reports { get; set; }
+    int    num_mentions { get; set; }
 
+    public persons(int id ,string first_name,string last_name,string secret_code,type type,int num_reports,int num_mentions)
+    {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.secret_code = secret_code;
+        this.type = type;
+        this.num_reports = num_reports;
+        this.num_mentions = num_mentions;
+    }
 }
---
+++ reports.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Reports
+{
+    int id { get; set; }
+    string text { get; set; }
+    int reportsId { get; set; }
+    int targetId { get; set; }
+    DateTime time { get; }
+
+    public Reports(int id, string text, int reportsId, int targetId)
+    {
+        this.id = id;
+        this.text = text;
+        this.reportsId = reportsId;
+        this.targetId = targetId;
+    }
+}
