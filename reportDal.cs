679bf6ae98a4c9ce4a50daba492a3df4a10bc32c
elazar matan <elazarmata8@gmail.com>
2025-06-11 17:45:03 +0300

    Add ReportDal class for database report operations
    Introduces the `ReportDal` class to manage database interactions for reports. Implements methods to update report and mention counts, and to insert new reports into the MySQL database. Includes error handling and ensures proper resource management. Fixes minor typographical errors in error messages and addresses SQL query increment issues.

---
+++ reportDal.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+public class ReportDal
+{
+    string constr = "server=localhost;username=root;password=;database=malshinon";
+    private MySqlConnection _con;
+
+    private void createConnection()
+    {
+        this._con = new MySqlConnection(constr);
+
+    }
+    private MySqlCommand comand(string query)
+    {
+        MySqlCommand cmd = new MySqlCommand(query, _con);
+        return cmd;
+    }
+
+    public ReportDal()
+    {
+        this.createConnection();
+    }
+    public void UpdateReportCount(int id)
+    {
+        try
+        {
+            _con.Open();
+            string query = $"UPDATE persons SET num_reports +1 WHERE id = @id";
+
+            var cmd = comand(query);
+
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.ExecuteNonQuery();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("EROR: " + e.Message);
+        }
+        finally
+        {
+            _con.Close();
+        }
+    }
+    public void UpdateMentionCount(int id)
+    {
+        try
+        {
+            _con.Open();
+            string query = $"UPDATE persons SET num_mentions +1 WHERE id = @id";
+
+            var cmd = comand(query);
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.ExecuteNonQuery();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("EROR: " + e.Message);
+        }
+        finally
+        {
+            _con.Close();
+        }
+    }
+    public void InsertIntelReport(Reports rep)
+    {
+        try
+        {
+            _con.Open();
+            string query = "INSERT INTO reports(reporter_id,target_id,text) VALUES (@reporter_id,@target_id,@text)";
+            var cmd = comand(query);
+            cmd.Parameters.AddWithValue("@reporter_id", rep.reportsId);
+            cmd.Parameters.AddWithValue("@target_id", rep.targetId);
+            cmd.Parameters.AddWithValue("@text", rep.text);
+
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("INSERT REPORT SUCESS!!!");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("EROR: " + e.Message);
+        }
+        finally
+        {
+            _con.Close();
+        }
+    }
+
+
+}
