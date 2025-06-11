03b5aa36136fa65a02224e8f9aac6329983551bd
elazar matan <elazarmata8@gmail.com>
2025-06-11 15:07:34 +0300

    Refactor ConsoleManager for improved user flow
    Enhanced the console application by introducing a new control variable for the initial loop, consolidating welcome messages into `Menu.enterToSystem()`, and streamlining the report creation process by directly calling `Menu.createReport()`. These changes improve code readability and maintainability.

--- consoleManager.cs
+++ consoleManager.cs
@@ -5,61 +5,20 @@ public class ConsoleManager
     public ConsoleManager()
     {
         PersonDal dal = new PersonDal();
-        bool exit = true;
-        while(exit)
+        bool v = true;
+        while (v)
         {
-            Console.WriteLine(
-                "======================================\n" +
-                "         WELCOME TO MALSHINON\n" +
-                "======================================\n"
-                  );
+
+            Menu.enterToSystem(); 
+            v = false;
+        }
+        bool exit = true;
+        while (exit)
+        {
+          
 
             Console.WriteLine(
-                "How do you want to enter?\n" +
-                "1. ful name\n" +
-                "2. secrete code\n"
-                );
-            string selectName = Console.ReadLine();
-            switch (selectName)
-            {
-                case "1":
-                    Console.WriteLine("enter first name");
-                    string firstName = Console.ReadLine();
-                    Console.WriteLine("enter last name");
-                    string lastName = Console.ReadLine();
-                    if (dal.GetPersonByName(firstName, lastName))
-                    {
-                        Console.WriteLine("YOU ARE INSIDE!!");
-                    }
-                    else
-                    {
-                        Console.WriteLine("You do not exist in the system, a new user has been created for you!");
-                        string scr = dal.createSecretCode(firstName);
-                        persons person = new persons(firstName,lastName,scr);
-                        dal.InsertNewPerson(person);
-                    }
-                    break;
-                case "2":
-                    Console.WriteLine("enter secret code");
-                    string secretCode = Console.ReadLine();
-                    if (dal.GetPersonBySecretCode(secretCode))
-                    {
-                        Console.WriteLine("YOU ARE INSIDE!!");
-                    }
-                    else
-                    {
-                        Console.WriteLine("you do not exist in the system please enter first name: ");
-                        string firstName1 = Console.ReadLine();
-                        Console.WriteLine("enter last name");
-                        string lastName2 = Console.ReadLine();
-                        persons pers = new persons(firstName1,lastName2,secretCode);
-                        dal.InsertNewPerson(pers);
-                    }
-                    break;
-            }
-
-            Console.WriteLine(
-                "      MENU\n"+
+                "      MENU\n" +
                 "________________\n" +
                 "1.create report \n" +
                 "2.exit"
@@ -68,7 +27,7 @@ public class ConsoleManager
             switch (selectChoice)
             {
                 case "1":
-                    Console.WriteLine("enter text");
+                    Menu.createReport();
                     break;
                 case "2":
                     exit = false;
@@ -76,6 +35,6 @@ public class ConsoleManager
             }
         }
     }
-
-
 }
+
+
