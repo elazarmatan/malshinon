a9ec8bec58c89b387aa9cf561bf815701224dc59
elazar matan <elazarmata8@gmail.com>
2025-06-11 16:53:26 +0300

    Enhance Menu class with user entry methods
    Added `enterByName` and `enterBySecretCode` methods to
    improve user interaction and error handling. Updated
    `createReport` for better prompts and feedback. Console
    color changes implemented for enhanced visual feedback.

--- menu.cs
+++ menu.cs
@@ -2,89 +2,131 @@
 
 static class Menu
 {
-    public static  void enterToSystem()
+    public static void enterByName()
     {
         PersonDal dal = new PersonDal();
-        Console.WriteLine(
-               "======================================\n" +
-               "         WELCOME TO MALSHINON\n" +
-               "======================================\n"
-                 );
 
-        Console.WriteLine(
-            "How do you want to enter?\n" +
-            "1. ful name\n" +
-            "2. secrete code\n"
-            );
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write("Enter first name: ");
+        string firstName = Console.ReadLine();
+        Console.Write("Enter last name: ");
+        string lastName = Console.ReadLine();
 
-        string selectName = Console.ReadLine();
-        switch (selectName)
+        if (dal.GetPersonByName(firstName, lastName))
         {
-            case "1":
-                Console.WriteLine("enter first name");
-                string firstName = Console.ReadLine();
-                Console.WriteLine("enter last name");
-                string lastName = Console.ReadLine();
-                if (dal.GetPersonByName(firstName, lastName))
-                {
-                    Console.WriteLine("YOU ARE INSIDE!!");
-                }
-                else
-                {
-                    Console.WriteLine("You do not exist in the system, a new user has been created for you!");
-                    string scr = dal.createSecretCode(firstName);
-                    persons person = new persons(firstName, lastName, scr);
-                    dal.InsertNewPerson(person);
-                }
-                break;
-            case "2":
-                Console.WriteLine("enter secret code");
-                string secretCode = Console.ReadLine();
-                if (dal.GetPersonBySecretCode(secretCode))
-                {
-                    Console.WriteLine("YOU ARE INSIDE!!");
-                }
-                else
-                {
-                    Console.WriteLine("you do not exist in the system please enter first name: ");
-                    string firstName1 = Console.ReadLine();
-                    Console.WriteLine("enter last name");
-                    string lastName2 = Console.ReadLine();
-                    persons pers = new persons(firstName1, lastName2, secretCode);
-                    dal.InsertNewPerson(pers);
-                }
-                break;
-
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Access granted.");
         }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("User not found.");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Creating new user...");
+            string scr = dal.createSecretCode(firstName);
+            string typer = "";
+            if (dal.getType("reporter", firstName, lastName))
+            {
+                typer = "reporter";
+            }
+            else
+            {
+                typer = "both";
+            }
+            persons person = new persons(firstName, lastName, scr,typer);
+            dal.InsertNewPerson(person);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"Generated secret code: {scr}");
+        }
+
+        Console.ResetColor();
     }
+
+
+    public static void enterBySecretCode()
+    {
+        PersonDal dal = new PersonDal();
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write("Enter secret code: ");
+        string secretCode = Console.ReadLine();
+
+        if (dal.GetPersonBySecretCode(secretCode))
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Access granted.");
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Code not recognized.");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Please enter your name to register.");
+            Console.Write("First name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Last name: ");
+            string lastName = Console.ReadLine();
+            string typer = "";
+            if (dal.getType("reporter", firstName, lastName))
+            {
+                typer = "reporter";
+            }
+            else
+            {
+                typer = "both";
+            }
+            persons pers = new persons(firstName, lastName, secretCode,typer);
+            dal.InsertNewPerson(pers);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("New user created successfully.");
+        }
+
+        Console.ResetColor();
+    }
+
+
 
     public static void createReport()
     {
         PersonDal dal = new PersonDal();
-        Console.WriteLine("enter first name");
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write("Enter your first name: ");
         string firstName = Console.ReadLine();
-        Console.WriteLine("enter last name");
+        Console.Write("Enter your last name: ");
         string lastName = Console.ReadLine();
-        Console.WriteLine("enter text");
+
+        Console.Write("Enter report text: ");
         string text = Console.ReadLine();
+
         fullName name1 = dal.NameExtraction(text);
         string first = name1.firstName;
         string last = name1.lastName;
+
         if (!dal.GetPersonByName(first, last))
         {
             string scr = dal.createSecretCode(last);
             if (!dal.SecretCodeExists(scr))
             {
-                persons person = new persons(first, last, scr, type.target);
+                persons person = new persons(first, last, scr,"target");
                 dal.InsertNewPerson(person);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine($"New target created with code: {scr}");
             }
-
         }
+
         int idtarg = dal.getIdByName(first, last);
         int idrep = dal.getIdByName(firstName, lastName);
+
         Reports rep = new Reports(text, idrep, idtarg);
         dal.InsertIntelReport(rep);
+
         dal.UpdateMentionCount(idtarg, 1);
         dal.UpdateReportCount(1, idrep);
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Report submitted successfully.");
+
+        Console.ResetColor();
     }
 }
