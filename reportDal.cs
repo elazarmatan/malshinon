f20cef26104d031f9644a9bee4c8e0b54047f02d
elazar matan <elazarmata8@gmail.com>
2025-06-12 16:30:38 +0300

    Remove Printreport method from ReportDal class
    The `Printreport` method has been completely removed from the `ReportDal` class in `reportDal.cs`. This method previously printed details about a person, including the reporter's name, target's name, report text, type of report, and the time taken to report. All related code has been deleted.

--- reportDal.cs
+++ reportDal.cs
@@ -115,18 +115,6 @@ public class ReportDal
     }
 
 
-    //public void Printreport()
-    //{
-    //    PersonDal dal = new PersonDal();
-
-
-    //    Console.WriteLine("========= Person Details =========");
-    //    Console.WriteLine($"Reporter:        {first_name}");
-    //    Console.WriteLine($"Target:          {last_name}");
-    //    Console.WriteLine($"Text:            {}");
-    //    Console.WriteLine($"Type:            {type}");
-    //    Console.WriteLine($"Time to report:  {num_reports}");
-    //    Console.WriteLine("==================================");
-    //}
+   
 
 }
